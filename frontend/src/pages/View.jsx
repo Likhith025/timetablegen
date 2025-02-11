c1bcb9470227670ee22106286c0885b6a838ebda
Likhith025 <alasyamlikhith2@gmail.com>
2025-02-11 10:39:09 +0530

    Changed Logo

--- frontend/src/pages/View.jsx
+++ frontend/src/pages/View.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import logo from '../assets/logo.png'
+import React from 'react';
+import logo from '../assets/logo.png';
 
 const View = () => {
   return (
     <div>
-        <div className="export-buttons">
-      <button className="csv-btn">Export as CSV</button>
-      <button className="pdf-btn">Export as PDF</button>
-      <button className="add-btn">Add</button>
-      <button >Signin Button</button>
+      <div className="export-buttons">
+        <button className="csv-btn">Export as CSV</button>
+        <button className="pdf-btn">Export as PDF</button>
+        <button className="add-btn">Add</button>
+        <button>Signin Button</button>
+      </div>
+      
+      {/* Image with styles to bring it to the front */}
+      <img 
+        src={logo} 
+        alt="Logo" 
+        style={{
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          width: '150px',
+          height: '150px',
+          zIndex: 9999
+        }}
+      />    </div>
+  );
+};
 
-    </div>
-      <img src={logo} alt="Data" />
-    </div>
-  )
-}
-
-export default View
+export default View;
