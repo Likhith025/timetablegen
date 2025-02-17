51896d244b537e47b49b07d65faef1c8f3c60247
Sathwik4804 <sathwikdachepally@gmail.com>
2025-02-17 12:03:32 +0530

    dcp

---
+++ timetablegen-main/frontend/src/Paths.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import './Button.css'
+import './Global.css'
+
+
+import Login from './pages/Login/Login';
+import OTP from './pages/Login/OTP';
+import View from './pages/View';
+
+
+const Paths = () => {
+  return (
+    <div>
+      <Routes>
+
+        <Route path='/' element={<Login/>}/>
+        <Route path='/otp' element={<OTP/>}/>
+        <Route path='/view' element={<View/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default Paths
---
+++ timetablegen-main/frontend/src/pages/Login/OTP.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OTP = () => {
+  return (
+    <div>
+      This is the OTP page
+    </div>
+  )
+}
+
+export default OTP
