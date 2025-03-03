f33311419913f1e9cd1c3c65e8e24914a52a5d65
Likhith025 <115356803+Likhith025@users.noreply.github.com>
2025-03-03 11:03:35 +0530

    Update src.js

--- frontend/src/src.js
+++ frontend/src/src.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "http://timetablegen-production-bb24.up.railway.app";
 const AP_BASE_URL = "http://tirumala-backend-vhzy.onrender.com";
 
 export default API_BASE_URL;
