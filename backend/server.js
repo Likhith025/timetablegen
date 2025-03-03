f258c77724557714b0f8f7dbfc73c0937a2dcd75
Likhith025 <115356803+Likhith025@users.noreply.github.com>
2025-03-03 11:00:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://timetablegen-production.up.railway.app"], 
+    origin: ["http://localhost:5173", "http://timetablegen-production-bb24.up.railway.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
